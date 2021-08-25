c9b8f3d8858659fb8d0419dc8f732251a084f021
Dipraj Howlader <diprajhowlader2015@gmail.com>
2021-08-25 21:01:24 +0600

    summing

--- Series1.java
+++ Series1.java
@@ -1,7 +1,22 @@
 package Series;
 
+import java.util.Scanner;
+
+
 public class Series1 {
 	public static void main(String[] args) {
+		// 1+2+3+4+5+........n
+		System.out.println("Enter the last digit of the loop:\n\n");
+		Scanner input = new Scanner(System.in);
+		int n = input.nextInt();
+		int sum=0;
+		for(int i = 0; i<=n; i++) {
+			System.out.printf("%d ", i);
+			sum =sum+i;
+		}
+		System.out.println("\n");
+		//if I want to get the sum, please wait
 		
+		System.out.println("The Sum is :" + sum);
 	}
 }
